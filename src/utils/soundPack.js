50fd5e3acc7527b81a787d956a727e4e5481788e
Xexi <hola@xexi.es>
2024-01-15 15:05:40 +0100

    New sound samples added

--- src/utils/soundPack.js
+++ src/utils/soundPack.js
@@ -1,13 +1,17 @@
-import kick from './../assets/sounds/kick.mp3'
-import snare from './../assets/sounds/snare.mp3'
-import chh from './../assets/sounds/chh.mp3'
-import ohh from './../assets/sounds/ohh.mp3'
+import kick from './../assets/sounds/club/floor hit 3.mp3'
+import snare from './../assets/sounds/club/909snare1.mp3'
+import chh from './../assets/sounds/club/808closehat.mp3'
+import ohh from './../assets/sounds/club/bright.mp3'
+import perc1 from './../assets/sounds/club/concrete.mp3'
+import perc2 from './../assets/sounds/club/blox slap.mp3'
 
 const soundPackCollection = {
     0: kick,
     1: snare,
     2: chh,
-    3: ohh
+    3: ohh,
+    4: perc1,
+    5: perc2
 }
 
 export default soundPackCollection
